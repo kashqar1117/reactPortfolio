ea7e4f02878b006d784a768a5dd575d9e31569c7
Kristina Al-ashqar <kristinaal-ashqar@Kristinas-MacBook-Air.local>
2021-04-19 21:19:00 -0500

    saving again

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import "./App.css";
+function App() {
+  return <h2>text</h2>;
+}
+
+export default App;
